38ccda3ead190e3f5b67e10792ed1c3cc6b5d572
Sam Priestley <sfpriestley@hotmail.co.uk>
2021-03-04 17:54:18 +0000

    30 work

---
+++ contracts/GenericLender/GenericAaveV2.sol
@@ -0,0 +1,176 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+import "./GenericLenderBase.sol";
+import "../Interfaces/Aave/IAToken.sol";
+import "../Interfaces/Aave/ILendingPool.sol";
+import "../Interfaces/Aave/IProtocolDataProvider.sol";
+import "../Interfaces/Aave/IReserveInterestRateStrategy.sol";
+import "../Libraries/Aave/DataTypes.sol";
+
+/********************
+ *   A lender plugin for LenderYieldOptimiser for any erc20 asset on Aave (not eth)
+ *   Made by SamPriestley.com
+ *   https://github.com/Grandthrax/yearnv2/blob/master/contracts/GenericLender/GenericCream.sol
+ *
+ ********************* */
+
+contract GenericAaveV2 is GenericLenderBase {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+    
+    uint256 private constant blocksPerYear = 2_300_000;
+
+    IProtocolDataProvider public immutable protocolDataProvider;
+    IAToken public immutable aToken;
+
+    constructor(
+        address _strategy,
+        string memory name,
+        IProtocolDataProvider _protocolDataProvider,
+        IAToken _aToken
+    ) public GenericLenderBase(_strategy, name) {
+        protocolDataProvider = _protocolDataProvider;
+        aToken = _aToken;
+
+        require(ILendingPool(_protocolDataProvider.ADDRESSES_PROVIDER().getLendingPool()).getReserveData(address(want)).aTokenAddress == address(_aToken), "WRONG ATOKEN");
+
+        want.approve(address(_protocolDataProvider.ADDRESSES_PROVIDER().getLendingPool()), type(uint256).max);
+    }
+
+    function nav() external view override returns (uint256) {
+        return _nav();
+    }
+
+    function _nav() internal view returns (uint256) {
+        return want.balanceOf(address(this)).add(underlyingBalanceStored());
+    }
+
+    function underlyingBalanceStored() public view returns (uint256 balance) {
+        balance = aToken.balanceOf(address(this));
+    }
+
+    function apr() external view override returns (uint256) {
+        return _apr();
+    }
+
+    function _apr() internal view returns (uint256) {
+        return uint(_lendingPool().getReserveData(address(want)).currentLiquidityRate).div(1e9); // dividing by 1e9 to pass from ray to wad
+    }
+
+    function weightedApr() external view override returns (uint256) {
+        uint256 a = _apr();
+        return a.mul(_nav());
+    }
+
+    function withdraw(uint256 amount) external override management returns (uint256) {
+        return _withdraw(amount);
+    }
+
+    //emergency withdraw. sends balance plus amount to governance
+    function emergencyWithdraw(uint256 amount) external override management {
+        _lendingPool().withdraw(address(want), amount, address(this));
+
+        want.safeTransfer(vault.governance(), want.balanceOf(address(this)));
+    }
+
+    //withdraw an amount including any want balance
+    function _withdraw(uint256 amount) internal returns (uint256) {
+        uint256 balanceUnderlying = aToken.balanceOf(address(this));
+        uint256 looseBalance = want.balanceOf(address(this));
+        uint256 total = balanceUnderlying.add(looseBalance);
+
+        if (amount > total) {
+            //cant withdraw more than we own
+            amount = total;
+        }
+
+        if (looseBalance >= amount) {
+            want.safeTransfer(address(strategy), amount);
+            return amount;
+        }
+
+        //not state changing but OK because of previous call
+        uint256 liquidity = want.balanceOf(address(aToken));
+
+        if (liquidity > 1) {
+            uint256 toWithdraw = amount.sub(looseBalance);
+
+            if (toWithdraw <= liquidity) {
+                //we can take all
+                _lendingPool().withdraw(address(want), toWithdraw, address(this));
+            } else {
+                //take all we can
+                _lendingPool().withdraw(address(want), liquidity, address(this));
+            }
+        }
+        looseBalance = want.balanceOf(address(this));
+        want.safeTransfer(address(strategy), looseBalance);
+        return looseBalance;
+    }
+
+    function deposit() external override management {
+        uint256 balance = want.balanceOf(address(this));
+        _lendingPool().deposit(address(want), balance, address(this), 7);
+    }
+
+    function withdrawAll() external override management returns (bool) {
+        uint256 invested = _nav();
+        uint256 returned = _withdraw(invested);
+        return returned >= invested;
+    }
+
+    function hasAssets() external view override returns (bool) {
+        return aToken.balanceOf(address(this)) > 0;
+    }
+
+    function _lendingPool() internal view returns (ILendingPool lendingPool) {
+        lendingPool = ILendingPool(protocolDataProvider.ADDRESSES_PROVIDER().getLendingPool());
+    }
+
+    function aprAfterDeposit(uint256 extraAmount) external view override returns (uint256) {
+        // i need to calculate new supplyRate after Deposit (when deposit has not been done yet)
+        DataTypes.ReserveData memory reserveData = _lendingPool().getReserveData(address(want));
+
+        (
+            uint availableLiquidity,
+            uint totalStableDebt,
+            uint totalVariableDebt,
+            ,
+            ,
+            ,
+            uint averageStableBorrowRate,
+            ,
+            ,
+            ) = protocolDataProvider.getReserveData(address(want));
+
+        uint newLiquidity = availableLiquidity.add(extraAmount);
+
+        (, , , , uint reserveFactor, , , , , ) = protocolDataProvider.getReserveConfigurationData(address(want));
+
+        (uint newLiquidityRate, , ) = IReserveInterestRateStrategy(reserveData.interestRateStrategyAddress).calculateInterestRates(
+            address(want),
+            newLiquidity,
+            totalStableDebt,
+            totalVariableDebt,
+            averageStableBorrowRate,
+            reserveFactor
+        );
+
+        return newLiquidityRate.div(1e9); // divided by 1e9 to go from Ray to Wad
+    }
+
+    function protectedTokens() internal view override returns (address[] memory) {
+        address[] memory protected = new address[](2);
+        protected[0] = address(want);
+        protected[1] = address(aToken);
+        return protected;
+    }
+}
--- tests/WETH/test_live_dai.py
+++ tests/WETH/test_live_dai.py
@@ -61,7 +61,8 @@ def test_030_live_dai(
     #strategy.setDebtThreshold(1_000_000 *1e18, {'from': strategist})
     #strategy.setProfitFactor(1000, {'from': strategist})
 
-
+    genericStateOfStrat(strategy, currency, vault)
+    genericStateOfVault(vault, currency)
 
 
     vault = Contract('0x19D3364A399d251E894aC732651be8B0E4e85001')
@@ -76,25 +77,29 @@ def test_030_live_dai(
     #strategy.addLender(dydxP, {"from": gov})
 
     #strategy.harvest({"from": strategist})
-    lev_comp = "0x4031afd3B0F71Bace9181E554A9E680Ee4AbE7dF"
-    alpha2 = "0x7D960F3313f3cB1BBB6BF67419d303597F3E2Fa8"
-    ib_lev_comp = "0x77b7CD137Dd9d94e7056f78308D7F65D2Ce68910"
-    gen_lender = "0x32b8C26d0439e1959CEa6262CBabC12320b384c4"
-    vault.updateStrategyDebtRatio(lev_comp, 6_000, {"from": gov})
-    vault.updateStrategyDebtRatio(alpha2, 2_000, {"from": gov})
-    vault.updateStrategyDebtRatio(ib_lev_comp, 100, {"from": gov})
-    vault.updateStrategyDebtRatio(gen_lender, 1_800, {"from": gov})
+    #lev_comp = "0x4031afd3B0F71Bace9181E554A9E680Ee4AbE7dF"
+    #alpha2 = "0x7D960F3313f3cB1BBB6BF67419d303597F3E2Fa8"
+    #ib_lev_comp = "0x77b7CD137Dd9d94e7056f78308D7F65D2Ce68910"
+    #gen_lender = "0x32b8C26d0439e1959CEa6262CBabC12320b384c4"
+    #vault.updateStrategyDebtRatio(lev_comp, 6_000, {"from": gov})
+    #vault.updateStrategyDebtRatio(alpha2, 2_000, {"from": gov})
+    #vault.updateStrategyDebtRatio(ib_lev_comp, 100, {"from": gov})
+    #vault.updateStrategyDebtRatio(gen_lender, 1_800, {"from": gov})
 
 
-    assert vault.debtRatio() == 9_900
+    #assert vault.debtRatio() == 9_900
     
     strategy.harvest({"from": strategist})
+
+    manualAll = [[dydxPlugin, 700], [creamPlugin, 300]]
+    strategy.manualAllocation(manualAll, {"from": strategist})
+    print("new alloc")
 
     genericStateOfStrat(strategy, currency, vault)
     genericStateOfVault(vault, currency)
 
-    form = "{:.2%}"
-    formS = "{:,.0f}"
+    #form = "{:.2%}"
+    #formS = "{:,.0f}"
 
     status = strategy.lendStatuses()
 
@@ -107,12 +112,62 @@ def test_030_live_dai(
     
 
     #vault.updateStrategyDebtRatio(strategy, 0, {'from': gov})
-    strategy.harvest({"from": strategist})
+    #strategy.harvest({"from": strategist})
     #genericStateOfStrat(strategy, currency, vault)
     #genericStateOfVault(vault, currency)
-    status = strategy.lendStatuses()
+    #status = strategy.lendStatuses()
 
-    for j in status:
-        print(
-            f"Lender: {j[0]}, Deposits: {formS.format(j[1]/1e18)}, APR: {form.format(j[2]/1e18)}"
-        )
+    #for j in status:
+    #    print(
+    #        f"Lender: {j[0]}, Deposits: {formS.format(j[1]/1e18)}, APR: {form.format(j[2]/1e18)}"
+    #    )
+
+
+def test_revoke_all(
+    dai,
+    interface,
+    samdev,
+    Contract,
+    devychad,
+    crDai,
+    Strategy,
+    daddy,
+    live_guest_list,
+    GenericDyDx,
+    GenericCream,
+    live_vault_dai_030,
+    live_strat_weth_032,
+    live_strat_dai_030,
+    live_dydxdai,
+    live_creamdai,
+    chain,
+    whale,
+    gov,
+    weth,
+    accounts,
+    rando,
+    fn_isolation,
+):
+
+    whale = accounts.at('0x014de182c147f8663589d77eadb109bf86958f13', force=True)
+    gov = daddy
+    currency = dai
+    decimals = currency.decimals()
+    strategist = samdev
+    #dydxPlugin = strategist.deploy(GenericDyDx, strategy, "DyDx")
+    #creamPlugin = strategist.deploy(GenericCream, strategy, "Cream", crDai)
+    dydxPlugin = live_dydxdai
+    creamPlugin = live_creamdai
+
+
+    vault = live_vault_dai_030
+    #tx = live_strat_weth_032.clone(vault, {'from': strategist})
+    #strategy = Strategy.at(tx.events['Cloned']["clone"])
+    strategy = Strategy.at(vault.withdrawalQueue(0))
+
+    vault.revokeStrategy(strategy, {'from': gov})
+    vault.removeStrategyFromQueue(s1, {'from': gov})
+    #vault.updateStrategyDebtRatio(strategy, 0, {'from': gov})
+    strategy.harvest({"from": strategist})
+    genericStateOfStrat(strategy, currency, vault)
+    genericStateOfVault(vault, currency)
