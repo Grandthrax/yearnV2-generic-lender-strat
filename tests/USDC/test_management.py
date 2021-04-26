92164cd120a3f70f3405a98945b10b29d2a1402e
jmonteer <jmonteer@protonmail.com>
2021-04-26 20:30:49 +0200

    feat: add test to set referral

--- tests/USDC/test_management.py
+++ tests/USDC/test_management.py
@@ -1,5 +1,5 @@
 from itertools import count
-from brownie import Wei, reverts
+from brownie import Wei, reverts, Contract
 from useful_methods import genericStateOfVault, genericStateOfStrat
 import random
 import brownie
@@ -41,3 +41,30 @@ def test_manual_override(
             plugin.deposit({"from": rando})
         with brownie.reverts("!management"):
             plugin.withdraw(1, {"from": rando})
+
+def test_set_referral(
+    strategy, chain, vault, currency, interface, whale, strategist, gov, rando, GenericAave
+):
+
+    decimals = currency.decimals()
+
+    deposit_limit = 100_000_000 * (10 ** decimals)
+    vault.addStrategy(strategy, 9800, 0, 2 ** 256 - 1, 500, {"from": gov})
+
+    amount1 = 50 * (10 ** decimals)
+    currency.approve(vault, 2 ** 256 - 1, {"from": whale})
+    currency.approve(vault, 2 ** 256 - 1, {"from": strategist})
+
+    vault.setDepositLimit(deposit_limit, {"from": gov})
+    assert vault.depositLimit() > 0
+
+    amount2 = 50_000 * (10 ** decimals)
+
+    vault.deposit(amount1, {"from": strategist})
+    vault.deposit(amount2, {"from": whale})
+    aave_lender = GenericAave.at(strategy.lenders(3))
+    aave_lender.setIsIncentivised(True, {'from': strategist}) # to increase APR 
+    aave_lender.setReferralCode(169, {'from': strategist})
+
+    tx = strategy.harvest({"from": strategist})
+    assert tx.events['Deposit']['referral'] == 169
